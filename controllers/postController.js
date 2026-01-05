5583ba057b2e73e513736c3ea570bfa8111b3d3c
Nitish43094 <www.nitish93369@gmail.com>
2026-01-05 20:38:05 +0545

    add post route

---
+++ controllers/postController.js
@@ -0,0 +1,25 @@
+const Post = require('../models/postModel')
+
+exports.createPost = async(req,res) =>{
+    try{
+        const {title,body} = req.body;
+        console.log(title," ",body)
+        if(!title || !body){
+            return res.status(400).json({
+                success:false,
+                message:"required the title and body"
+            })
+        }
+        const post = await Post.create({title,body});
+        res.status(201).json({
+            success:true,
+            data:post,
+        })
+    }catch(err){
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: "Post create करते समय error आया",
+        });
+    }
+}
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
-const dbConnection = require('./config/dbConnect')
+const dbConnection = require('./config/dbConnect');
+const route = require('./routes/blogs');
 const app = express();
 app.use(express.json())
 require('dotenv').config();
 const PORT = process.env.PORT || 4000
 dbConnection();
+
 app.get('/',(req,res)=>{
     res.send(`<h1>This is backend home page and added database and models</h1>`)
 })
-
+app.use("/api/v1",route)
 app.listen(PORT,()=>{
     console.log("The server started at Port ",PORT)
 })
