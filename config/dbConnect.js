9884af79f2c2a8a471a5ae4191bbe406cc20696d
Nitish43094 <www.nitish93369@gmail.com>
2026-01-05 21:06:44 +0545

    change dbConnect

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,15 +1,23 @@
 const mongoose = require("mongoose")
 require('dotenv').config()
+let isConnected = false;
 
 const dbConnection = async()=>{
-    mongoose.connect(process.env.DATABASE_URL)
-    .then(()=>{
-        console.log("Connection Successfully")
-    })
-    .catch((error) =>{
-        console.log("Database Connection Error")
-        console.error(error.message)
-    })
+    if (isConnected) {
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.DATABASE_URL, {
+            bufferCommands: false, // 🔥 IMPORTANT
+        });
+
+        isConnected = db.connections[0].readyState;
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.error("MongoDB connection failed:", error.message);
+        throw error;
+    }
 }
 
 module.exports = dbConnection;
